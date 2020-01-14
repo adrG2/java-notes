bf88f2a7591522e841f73ea62810e5a67d3feef5
Adrian Garcia <adrian.garcia@gmail.com>
2020-01-14 12:25:17 +0100

    Creado submodulo para explicacion de la directiva static

---
+++ static-directive/src/main/java/com/github/staticDirective/StaticDirectiveApplication.java
@@ -0,0 +1,11 @@
+package com.github.generics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticDirectiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaticDirectiveApplication.class, args);
+    }
+}
