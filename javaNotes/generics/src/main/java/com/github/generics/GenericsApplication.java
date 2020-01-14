65a7f422d3142c43df790a942b4b58da8163636b
Adrian Garcia <adrian.garcia@gmail.com>
2020-01-14 10:44:04 +0100

    Configuracion inicial
    Cambio versión a java 12 por error de compilacion. Se añade subomodule de generics

---
+++ javaNotes/generics/src/main/java/com/github/generics/GenericsApplication.java
@@ -0,0 +1,9 @@
+package com.github.generics;
+
+
+@SpringBootApplication
+public class GenericsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenericsApplication.class, args);
+    }
+}
