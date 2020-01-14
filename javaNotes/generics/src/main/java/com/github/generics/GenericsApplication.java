1d8557bf4657d1739bf138e879df73751536f840
Adrian Garcia <adrian.garcia@gmail.com>
2020-01-14 10:46:56 +0100

    Importamos dependencias de spring para el submodule

--- javaNotes/generics/src/main/java/com/github/generics/GenericsApplication.java
+++ javaNotes/generics/src/main/java/com/github/generics/GenericsApplication.java
@@ -1,5 +1,7 @@
 package com.github.generics;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenericsApplication {
