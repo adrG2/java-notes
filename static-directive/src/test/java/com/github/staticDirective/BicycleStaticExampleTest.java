07b23b97b5a96aafd678bedaad6243b335d95a8e
Adrian Garcia <adrian.garcia@gmail.com>
2020-01-15 09:15:26 +0100

    Aclaracion en el test para diferenciar variable de instancia de la de clase

--- static-directive/src/test/java/com/github/staticDirective/BicycleStaticExampleTest.java
+++ static-directive/src/test/java/com/github/staticDirective/BicycleStaticExampleTest.java
@@ -1,14 +1,9 @@
 package com.github.staticDirective;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsInstanceOf.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
 
 import com.github.staticDirective.lib.Bicycle;
-import com.github.staticDirective.lib.SingletonTest;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,6 +23,8 @@ public class BicycleStaticExampleTest {
         assertEquals(1, bike.getID());
         Bicycle secondBike = new Bicycle(5, 2, 12);
         assertNotEquals(1, secondBike.getID());
+        // Esto es así porque el id es una variable de instancia de clase. Se inicializa
+        // a 0 para cada instancia.
         assertEquals(1, bike.getID());
         assertEquals(2, Bicycle.getNumberOfBicycles());
 
