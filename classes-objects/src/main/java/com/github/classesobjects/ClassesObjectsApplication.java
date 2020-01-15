38ad2a6316ec7eeb66716fa3113ef63aed7c05df
Adrian Garcia <adrian.garcia@gmail.com>
2020-01-15 10:15:27 +0100

    Añadida configuracion para submodulo classes-objects

---
+++ classes-objects/src/main/java/com/github/classesobjects/ClassesObjectsApplication.java
@@ -0,0 +1,11 @@
+package com.github.classesobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassesObjectsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassesObjectsApplication.class, args);
+    }
+}
